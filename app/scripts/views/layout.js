2349d019af63b35f4040b508139443185eb7a486
frebro <frebro@gmail.com>
2013-10-10 22:01:02 +0200

    jshint cleanup

--- app/scripts/views/layout.js
+++ app/scripts/views/layout.js
@@ -1,61 +1,70 @@
 /*global define*/
-
 define([
     'jquery',
     'underscore',
     'backbone',
     'templates'
 ], function ($, _, Backbone, JST) {
+
     'use strict';
 
     /*
     var LayoutView = Backbone.View.extend({
-        template: JST['app/scripts/templates/layout.ejs']
+    template: JST['app/scripts/templates/layout.ejs']
     });
-	*/
+    */
     var View = {};
 
     //Temporary menu sollution. To be moved either to a template or a collection.
     var menuItems = {
-		menuItems: [
-			{title: 'Reports', href: '#/reports'},
-			{title: 'Add new', href: '#/reports/add'},
-			{title: 'User', href: '#/user/1'}
-		]
+        menuItems: [
+            {
+                title: 'Reports',
+                href: '#/reports'
+            },
+            {
+                title: 'Add new',
+                href: '#/reports/add'
+            },
+            {
+                title: 'User',
+                href: '#/user/1'
+            }
+        ]
     };
 
     View.MainLayout = Backbone.View.extend({
-		el: '#container',
-		template: JST['app/scripts/templates/layout.ejs'],
-		render: function() {
-			this.$el.html(this.template);
+        el: '#container',
+        template: JST['app/scripts/templates/layout.ejs'],
+        render: function() {
+            this.$el.html(this.template);
 
-			var menu = new View.Menu();
-			menu.render();
+            var menu = new View.Menu();
+            menu.render();
 
-			var footer = new View.Footer();
-			footer.render();
-			return this;
-		}
+            var footer = new View.Footer();
+            footer.render();
+            return this;
+        }
     });
 
     View.Menu = Backbone.View.extend({
-		el: '#header',
-		template: JST['app/scripts/templates/menu.ejs'],
-		render: function() {
-			this.$el.html(this.template(menuItems));
-			return this;
-		}
-	});
+        el: '#header',
+        template: JST['app/scripts/templates/menu.ejs'],
+        render: function() {
+            this.$el.html(this.template(menuItems));
+            return this;
+        }
+    });
 
-	View.Footer = Backbone.View.extend({
-		el: '#footer',
-		template: JST['app/scripts/templates/footer.ejs'],
-		render: function() {
-			this.$el.html(this.template);
-			return this;
-		}
-	});
+    View.Footer = Backbone.View.extend({
+        el: '#footer',
+        template: JST['app/scripts/templates/footer.ejs'],
+        render: function() {
+            this.$el.html(this.template);
+            return this;
+        }
+    });
 
     return View;
 });
--- app/scripts/views/reports.js
+++ app/scripts/views/reports.js
@@ -1,5 +1,4 @@
 /*global define*/
-
 define([
     'jquery',
     'underscore',
@@ -7,50 +6,55 @@ define([
     'templates',
     'views/map'
 ], function ($, _, Backbone, JST, Map) {
+
     'use strict';
 
     var Reports = {};
 
     var ReportData = {
-		reports: [
-			{'title': 'test report'},
-			{'title': 'test report2'}
-		]
+        reports: [
+            {
+                'title': 'test report'
+            },
+            {
+                'title': 'test report2'
+            }
+        ]
     };
 
     Reports.MapLayout = Backbone.View.extend({
-		el: '#content',
-		template: JST['app/scripts/templates/mapLayout.ejs'],
-		render: function() {
-			this.$el.html(this.template);
+        el: '#content',
+        template: JST['app/scripts/templates/mapLayout.ejs'],
+        render: function() {
+            this.$el.html(this.template);
 
-			var map = new Map();
-			map.render();
+            var map = new Map();
+            map.render();
 
-			var list = new Reports.List({'model': ReportData});
-			list.render();
+            var list = new Reports.List({'model': ReportData});
+            list.render();
 
-			return this;
-		}
-	});
+            return this;
+        }
+    });
 
-	Reports.Map = Backbone.View.extend({
-		el: '#mapContainer',
-		template: JST['app/scripts/templates/map.ejs'],
-		render: function() {
-			this.$el.html('mapContainer');
-			return this;
-		}
-	});
+    Reports.Map = Backbone.View.extend({
+        el: '#mapContainer',
+        template: JST['app/scripts/templates/map.ejs'],
+        render: function() {
+            this.$el.html('mapContainer');
+            return this;
+        }
+    });
 
-	Reports.List = Backbone.View.extend({
-		el: '#reports',
-		template: JST['app/scripts/templates/reportsList.ejs'],
-		render: function() {
-			this.$el.html(this.template(this.model));
-			return this;
-		}
-	});
+    Reports.List = Backbone.View.extend({
+        el: '#reports',
+        template: JST['app/scripts/templates/reportsList.ejs'],
+        render: function() {
+            this.$el.html(this.template(this.model));
+            return this;
+        }
+    });
 
     return Reports;
 });
