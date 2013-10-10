09e1f9c5196d986ef3ccfca34645fc325bf8d46f
AlexxHansson <alexxhansson@gmail.com>
2013-10-10 13:22:17 +0200

    Templating deluxe

--- app/scripts/routes/main.js
+++ app/scripts/routes/main.js
@@ -2,23 +2,26 @@
 
 define([
     'jquery',
-    'backbone'
-], function ($, Backbone) {
+    'backbone',
+    'views/layout'
+], function ($, Backbone, Layout) {
     'use strict';
 
     var MainRouter = Backbone.Router.extend({
         routes: {
-            "": "index",
-            "user": "user",
-            "user/:id": "user"
+            '': 'index',
+            'user': 'user',
+            'user/:id': 'user'
         },
 
         index: function() {
-            console.log("Welcome to your main router.");
+            var mainLayout = new Layout.MainLayout();
+            mainLayout.render();
+            console.log('Welcome to your main router.');
         },
 
         user: function(id) {
-            console.log("Welcome to your user router.");
+            console.log('Welcome to your user router.');
             console.log(id);
         }
 
---
+++ app/scripts/views/layout.js
@@ -0,0 +1,55 @@
+/*global define*/
+
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'templates'
+], function ($, _, Backbone, JST) {
+    'use strict';
+
+    /*
+    var LayoutView = Backbone.View.extend({
+        template: JST['app/scripts/templates/layout.ejs']
+    });
+	*/
+    var View = {};
+
+    View.MainLayout = Backbone.View.extend({
+		el: '#container',
+		template: JST['app/scripts/templates/layout.ejs'],
+		render: function() {
+			console.log(this.template);
+			//Old compile
+			//var compiledTemplate = _.template( this.template );
+			this.$el.html(this.template);
+
+			var menu = new View.Menu();
+			menu.render();
+
+			var footer = new View.Footer();
+			footer.render();
+			return this;
+		}
+    });
+
+    View.Menu = Backbone.View.extend({
+		el: '#header',
+		template: JST['app/scripts/templates/menu.ejs'],
+		render: function() {
+			this.$el.html(this.template({title: 'Reports'}));
+			return this;
+		}
+	});
+
+	View.Footer = Backbone.View.extend({
+		el: '#footer',
+		template: JST['app/scripts/templates/footer.ejs'],
+		render: function() {
+			this.$el.html(this.template);
+			return this;
+		}
+	});
+
+    return View;
+});
