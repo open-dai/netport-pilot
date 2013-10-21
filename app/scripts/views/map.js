19f3ec8dda7ebae4506b903b341594f92e5ea267
frebro <frebro@gmail.com>
2013-10-21 15:06:49 +0200

    more semantic naming of app components

--- app/scripts/views/map.js
+++ app/scripts/views/map.js
@@ -9,11 +9,11 @@ define([
     'use strict';
 
     var MapView = Backbone.View.extend({
-        el: '#mapContainer',
+        el: '#map',
         render: function() {
             console.log('render map');
 
-            var map = L.map('mapContainer').setView([56.169401778813686, 14.864437580108644], 13);
+            var map = L.map('map').setView([56.169401778813686, 14.864437580108644], 13);
             L.tileLayer('http://{s}.tile.cloudmade.com/4e5f745e28654b7eb26aab577eed79ee/997/256/{z}/{x}/{y}.png', {
                 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>[…]',
                 maxZoom: 18
