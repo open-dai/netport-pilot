bf10faaf1641a5b4a128dfb996062d1a007ef831
AlexxHansson <alexxhansson@gmail.com>
2013-11-07 15:23:13 +0100

    Map on create report
    Must wait to load. Need some feedback.

--- app/scripts/views/reports.js
+++ app/scripts/views/reports.js
@@ -1,5 +1,6 @@
 /*global define*/
 define([
+    'app',
     'jquery',
     'underscore',
     'backbone',
@@ -7,7 +8,7 @@ define([
     'models/report',
     'models/user',
     'views/map'
-], function ($, _, Backbone, JST, ReportModel, UserModel, MapView) {
+], function (App, $, _, Backbone, JST, ReportModel, UserModel, MapView) {
 
     'use strict';
 
@@ -42,36 +43,39 @@ define([
             'click button': 'save'
         },
         initialize: function() {
+            this.model = new ReportModel();
+            this.model.url = 'http://localhost:8001/api/reports';
+            _.bindAll(this);
+        },
+        showPosition: function(position) {
+            this.model.set({
+                'lat': position.coords.latitude,
+                'lng': position.coords.longitude
+            });
+            this.mapRender();
+        },
+        render: function() {
             if (navigator.geolocation)
             {
                 navigator.geolocation.getCurrentPosition(this.showPosition);
             }
+            this.$el.html(this.template({'types': this.collection.toJSON()}));
+            return this;
         },
-        showPosition: function(position) {
-            $('#lat').val(position.coords.latitude);
-            $('#lng').val(position.coords.longitude);
-        },
-        render: function() {
-            var login = UserModel.checkLogin();
-            if( login === true ) {
-                this.$el.html(this.template({'types': this.collection.toJSON()}));
-            
-                return this;
-            }
+        mapRender: function() {
+            var mapView = new MapView.MapView({model: this.model});
+            mapView.render();
         },
         save: function() {
             console.log('Saving data');
-            var newReport = new ReportModel();
-            newReport.url = 'http://localhost:8001/api/reports';
-            newReport.set({
+            this.model.set({
                 'description': $('#description').val(),
                 'types_id': $('#types_id').val(),
-                'lat': $('#lat').val(),
-                'lng': $('#lng').val(),
                 'fb_id': UserModel.get('fb_id')
             });
-
-            newReport.save();
+            this.model.save({success: function(){
+                App.router.navigate('#/reports', {trigger: true});
+            }});
         }
     });
 
