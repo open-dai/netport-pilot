af169ebff6383b832ee7ddc1e66ae627577bb447
AlexxHansson <alexxhansson@gmail.com>
2014-03-25 14:58:12 +0100

    Deploy preparation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,4 +7,7 @@ define(function(require, exports, module) {
 
     // The root path to run the application through.
     app.root = '/';
+    
+    app.api = 'http://localhost:8888';
+    //app.api = 'http://194.116.110.159:8888';
 });
